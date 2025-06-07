7930b4f0b34891092de9d2082a99559a7a9bbab8
deepanshu-dev <deepanshur88@gmail.com>
2025-06-07 12:44:00 +0530

    added new playlist

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const API_KEY = "AIzaSyALr9tR1bj7ZTvD_K_jrWJqAaZiRUNRlSQ";
-
 // Fetch playlists from json file
 async function getPlaylists() {
   let playlists = [];
@@ -39,34 +37,6 @@ function displayPlaylists(playlists) {
 async function fetchAllPlaylistsDetails() {
   const playlists = await getPlaylists();
   displayPlaylists(playlists);
-
-  // const playlistDetails = [];
-  // for (let playlist of playlists) {
-  //   // Extract playlist ID from URL
-  //   const playlistId = new URL(playlist.streamURL).searchParams.get("list");
-  //   const apiUrl = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=${playlistId}&key=${API_KEY}`;
-
-  //   try {
-  //     const response = await fetch(apiUrl);
-  //     if (!response.ok) {
-  //       throw new Error('Network response was not ok');
-  //     }
-
-  //     const data = await response.json();
-  //     if (data.items && data.items.length > 0) {
-  //       playlistDetails.push(
-  //         {
-  //           ...data.items[0].snippet,
-  //           poster: data.items[0].snippet.thumbnails.maxres.url,
-  //           streamURL: `https://www.youtube.com/playlist?list=${data.items[0].id}`
-  //         }
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.error(`Error fetching playlist with ID ${playlistId}:`, error);
-  //   }
-  // }
-  // displayPlaylists(playlistDetails);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
